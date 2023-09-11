70de9c9071cfb173babe24eb6f27eeef9861f5bc
jda1313 <142521753+jda1313@users.noreply.github.com>
2023-09-11 14:47:27 -0300

    Name and Age prog
    Test of program to enter name and age

---
+++ main.py
@@ -0,0 +1,12 @@
+def main():
+    # Prompt user for their name
+    name = input("Please enter your name: ")
+
+    # Prompt user for their age
+    age = int(input("Please enter your age: "))
+
+    # Greet the user and display their age
+    print(f"Hello, {name}! You are {age} years old.")
+
+if __name__ == "__main__":
+    main()
